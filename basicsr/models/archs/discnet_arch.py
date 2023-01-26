77d8c4360d9ad5e3e6c41046541d7115681d5bc9
Chanwoo Park <chanwoo@aces.snu.ac.kr>
2023-01-26 19:56:27 +0900

    chore: merge conflict

--- basicsr/models/archs/discnet_arch.py
+++ basicsr/models/archs/discnet_arch.py
@@ -10,9 +10,9 @@ import math
 
 
 class DISCNet(nn.Module):
-    def __init__(self, in_nc, out_nc, nf, ns, kpn_sz=5, norm_type=None, \
-                    act_type='leakyrelu', res_scale=1, kernel_size=3, \
-                    kernel_cond=None, psf_nc=5, multi_scale=False):
+    def __init__(self, in_nc, out_nc, nf, ns, kpn_sz=5, norm_type=None,
+                 act_type='leakyrelu', res_scale=1, kernel_size=3,
+                 kernel_cond=None, psf_nc=5, multi_scale=False):
         super().__init__()
         self.ns = ns
         self.kpn_sz = kpn_sz
@@ -24,28 +24,34 @@ class DISCNet(nn.Module):
         # Restoration Branch
         #############################
         # Encoder
-        self.conv_11 = conv_block(in_nc, nf, kernel_size=kernel_size, act_type=act_type)
+        self.conv_11 = conv_block(
+            in_nc, nf, kernel_size=kernel_size, act_type=act_type)
         self.conv_12 = ResBlock(nf, res_scale=res_scale, act_type=act_type)
         self.conv_13 = ResBlock(nf, res_scale=res_scale, act_type=act_type)
 
-        self.conv_21 = conv_block(nf, 2*nf, stride=2, kernel_size=kernel_size, act_type=act_type)
-        self.conv_22 = ResBlock(2*nf, res_scale=res_scale, act_type=act_type)
-        self.conv_23 = ResBlock(2*nf, res_scale=res_scale, act_type=act_type)
+        self.conv_21 = conv_block(
+            nf, 2 * nf, stride=2, kernel_size=kernel_size, act_type=act_type)
+        self.conv_22 = ResBlock(2 * nf, res_scale=res_scale, act_type=act_type)
+        self.conv_23 = ResBlock(2 * nf, res_scale=res_scale, act_type=act_type)
 
-        self.conv_31 = conv_block(2*nf, 4*nf, stride=2, kernel_size=kernel_size, act_type=act_type)
-        self.conv_32 = ResBlock(4*nf, res_scale=res_scale, act_type=act_type)
-        self.conv_33 = ResBlock(4*nf, res_scale=res_scale, act_type=act_type)
+        self.conv_31 = conv_block(
+            2 * nf, 4 * nf, stride=2, kernel_size=kernel_size, act_type=act_type)
+        self.conv_32 = ResBlock(4 * nf, res_scale=res_scale, act_type=act_type)
+        self.conv_33 = ResBlock(4 * nf, res_scale=res_scale, act_type=act_type)
 
         # Decoder
-        self.upconv_21 = upconv(4*nf, 2*nf, 2, act_type=act_type)
-        self.upconv_22 = ResBlock(2*nf, res_scale=res_scale, act_type=act_type)
-        self.upconv_23 = ResBlock(2*nf, res_scale=res_scale, act_type=act_type)
+        self.upconv_21 = upconv(4 * nf, 2 * nf, 2, act_type=act_type)
+        self.upconv_22 = ResBlock(
+            2 * nf, res_scale=res_scale, act_type=act_type)
+        self.upconv_23 = ResBlock(
+            2 * nf, res_scale=res_scale, act_type=act_type)
 
-        self.upconv_11 = upconv(2*nf, nf, 2, act_type=act_type)
+        self.upconv_11 = upconv(2 * nf, nf, 2, act_type=act_type)
         self.upconv_12 = ResBlock(nf, res_scale=res_scale, act_type=act_type)
         self.upconv_13 = ResBlock(nf, res_scale=res_scale, act_type=act_type)
 
-        self.final_conv = conv_block(nf, out_nc, kernel_size=kernel_size, act_type=act_type)
+        self.final_conv = conv_block(
+            nf, out_nc, kernel_size=kernel_size, act_type=act_type)
 
         #############################
         # Kernel Prediction Branch
@@ -58,9 +64,13 @@ class DISCNet(nn.Module):
             elif self.kernel_cond == 'img-psf':
                 cond_nc = in_nc + psf_nc
 
-            self.kconv_11 = conv_block(cond_nc, nf, kernel_size=kernel_size, act_type=act_type)
-            self.kconv_12 = ResBlock(nf, res_scale=res_scale, act_type=act_type)
-            self.kconv_13 = ResBlock(nf, res_scale=res_scale, act_type=act_type)
+            # Here: where cond_nc is in_nc (which is 3 in DISCNet) + psf_nc (PCA dimension of PSF)
+            self.kconv_11 = conv_block(
+                cond_nc, nf, kernel_size=kernel_size, act_type=act_type)
+            self.kconv_12 = ResBlock(
+                nf, res_scale=res_scale, act_type=act_type)
+            self.kconv_13 = ResBlock(
+                nf, res_scale=res_scale, act_type=act_type)
 
             if self.multi_scale:
                 self.dynamic_kernel1 = nn.Sequential(
@@ -69,27 +79,34 @@ class DISCNet(nn.Module):
                     ResBlock(nf, res_scale=res_scale, act_type=act_type),
                     conv_block(nf, nf * (kpn_sz ** 2), kernel_size=1))
 
-            self.kconv_21 = conv_block(nf, 2*nf, stride=2, kernel_size=kernel_size, act_type=act_type)
-            self.kconv_22 = ResBlock(2*nf, res_scale=res_scale, act_type=act_type)
-            self.kconv_23 = ResBlock(2*nf, res_scale=res_scale, act_type=act_type)
+            self.kconv_21 = conv_block(
+                nf, 2 * nf, stride=2, kernel_size=kernel_size, act_type=act_type)
+            self.kconv_22 = ResBlock(
+                2 * nf, res_scale=res_scale, act_type=act_type)
+            self.kconv_23 = ResBlock(
+                2 * nf, res_scale=res_scale, act_type=act_type)
 
             if self.multi_scale:
                 self.dynamic_kernel2 = nn.Sequential(
-                    conv_block(2*nf, 2*nf, kernel_size=kernel_size),
-                    ResBlock(2*nf, res_scale=res_scale, act_type=act_type),
-                    ResBlock(2*nf, res_scale=res_scale, act_type=act_type),
-                    conv_block(2*nf, 2*nf * (kpn_sz ** 2), kernel_size=1))
+                    conv_block(2 * nf, 2 * nf, kernel_size=kernel_size),
+                    ResBlock(2 * nf, res_scale=res_scale, act_type=act_type),
+                    ResBlock(2 * nf, res_scale=res_scale, act_type=act_type),
+                    conv_block(2 * nf, 2 * nf * (kpn_sz ** 2), kernel_size=1))
 
-            self.kconv_31 = conv_block(2*nf, 4*nf, stride=2, kernel_size=kernel_size, act_type=act_type)
-            self.kconv_32 = ResBlock(4*nf, res_scale=res_scale, act_type=act_type)
-            self.kconv_33 = ResBlock(4*nf, res_scale=res_scale, act_type=act_type)
+            self.kconv_31 = conv_block(
+                2 * nf, 4 * nf, stride=2, kernel_size=kernel_size, act_type=act_type)
+            self.kconv_32 = ResBlock(
+                4 * nf, res_scale=res_scale, act_type=act_type)
+            self.kconv_33 = ResBlock(
+                4 * nf, res_scale=res_scale, act_type=act_type)
 
             self.dynamic_kernel = nn.Sequential(
-                conv_block(4*nf, 4*nf, kernel_size=kernel_size),
-                ResBlock(4*nf, res_scale=res_scale, act_type=act_type),
-                ResBlock(4*nf, res_scale=res_scale, act_type=act_type),
-                conv_block(4*nf, 4*nf * (kpn_sz ** 2), kernel_size=1))
+                conv_block(4 * nf, 4 * nf, kernel_size=kernel_size),
+                ResBlock(4 * nf, res_scale=res_scale, act_type=act_type),
+                ResBlock(4 * nf, res_scale=res_scale, act_type=act_type),
+                conv_block(4 * nf, 4 * nf * (kpn_sz ** 2), kernel_size=1))
 
+    self.previous_parameter = None
 
     def forward(self, x, psf=None):
         if not self.training:
@@ -102,6 +119,8 @@ class DISCNet(nn.Module):
         # Kernel Prediction Branch
         #############################
         # kernel network
+
+        # here: where we concat PSF kernel code
         if self.kernel_cond:
             if self.kernel_cond == 'img':
                 cond_x = x
@@ -119,6 +138,8 @@ class DISCNet(nn.Module):
                 dynamic_kernel2 = self.dynamic_kernel2(kfea2)
             dynamic_kernel = self.dynamic_kernel(kfea3)
 
+            print(self.kconv_11._modules['0'].__dict__)
+
         #############################
         # Restoration Branch
         #############################
@@ -134,15 +155,19 @@ class DISCNet(nn.Module):
         # Decoder
         if self.multi_scale:
             fea2 = kernel2d_conv(fea2, dynamic_kernel2, self.kpn_sz)
-            upfea2 = self.upconv_23(self.upconv_22(self.upconv_21(fea3) + fea2))
+            upfea2 = self.upconv_23(
+                self.upconv_22(self.upconv_21(fea3) + fea2))
         else:
-            upfea2 = self.upconv_23(self.upconv_22(self.upconv_21(fea3) + fea2))
+            upfea2 = self.upconv_23(
+                self.upconv_22(self.upconv_21(fea3) + fea2))
 
         if self.multi_scale:
             fea1 = kernel2d_conv(fea1, dynamic_kernel1, self.kpn_sz)
-            upfea1 = self.upconv_13(self.upconv_12(self.upconv_11(upfea2) + fea1))
+            upfea1 = self.upconv_13(self.upconv_12(
+                self.upconv_11(upfea2) + fea1))
         else:
-            upfea1 = self.upconv_13(self.upconv_12(self.upconv_11(upfea2) + fea1))
+            upfea1 = self.upconv_13(self.upconv_12(
+                self.upconv_11(upfea2) + fea1))
 
         fea = self.final_conv(upfea1)
         out = fea + x
@@ -152,7 +177,7 @@ class DISCNet(nn.Module):
         return out
 
 
-def conv_block(in_nc, out_nc, kernel_size, stride=1, dilation=1, groups=1, bias=True, \
+def conv_block(in_nc, out_nc, kernel_size, stride=1, dilation=1, groups=1, bias=True,
                pad_type='zero', norm_type=None, act_type='prelu', mode='CNA'):
     '''
     Conv layer with padding, normalization, activation
@@ -164,8 +189,8 @@ def conv_block(in_nc, out_nc, kernel_size, stride=1, dilation=1, groups=1, bias=
     p = pad(pad_type, padding) if pad_type and pad_type != 'zero' else None
     padding = padding if pad_type == 'zero' else 0
 
-    c = nn.Conv2d(in_nc, out_nc, kernel_size=kernel_size, stride=stride, padding=padding, \
-            dilation=dilation, bias=bias, groups=groups)
+    c = nn.Conv2d(in_nc, out_nc, kernel_size=kernel_size, stride=stride, padding=padding,
+                  dilation=dilation, bias=bias, groups=groups)
     a = act(act_type) if act_type else None
     if 'CNA' in mode:
         n = norm(norm_type, out_nc) if norm_type else None
@@ -180,6 +205,7 @@ def conv_block(in_nc, out_nc, kernel_size, stride=1, dilation=1, groups=1, bias=
         n = norm(norm_type, in_nc) if norm_type else None
         return sequential(n, a, p, c)
 
+
 def act(act_type, inplace=True, neg_slope=0.2, n_prelu=1):
     # helper selecting activation
     # neg_slope: for leakyrelu and init of prelu
@@ -192,8 +218,10 @@ def act(act_type, inplace=True, neg_slope=0.2, n_prelu=1):
     elif act_type == 'prelu':
         layer = nn.PReLU(num_parameters=n_prelu, init=neg_slope)
     else:
-        raise NotImplementedError('activation layer [{:s}] is not found'.format(act_type))
+        raise NotImplementedError(
+            'activation layer [{:s}] is not found'.format(act_type))
     return layer
+
 
 def norm(norm_type, nc):
     # helper selecting normalization layer
@@ -203,8 +231,10 @@ def norm(norm_type, nc):
     elif norm_type == 'instance':
         layer = nn.InstanceNorm2d(nc, affine=False)
     else:
-        raise NotImplementedError('normalization layer [{:s}] is not found'.format(norm_type))
+        raise NotImplementedError(
+            'normalization layer [{:s}] is not found'.format(norm_type))
     return layer
+
 
 def pad(pad_type, padding):
     # helper selecting padding layer
@@ -217,31 +247,35 @@ def pad(pad_type, padding):
     elif pad_type == 'replicate':
         layer = nn.ReplicationPad2d(padding)
     else:
-        raise NotImplementedError('padding layer [{:s}] is not implemented'.format(pad_type))
+        raise NotImplementedError(
+            'padding layer [{:s}] is not implemented'.format(pad_type))
     return layer
+
 
 def get_valid_padding(kernel_size, dilation):
     kernel_size = kernel_size + (kernel_size - 1) * (dilation - 1)
     padding = (kernel_size - 1) // 2
     return padding
 
-def upconv(in_nc, out_nc, upscale_factor=2, kernel_size=3, stride=1, bias=True, \
-                        pad_type='zero', norm_type=None, act_type='relu'):
+
+def upconv(in_nc, out_nc, upscale_factor=2, kernel_size=3, stride=1, bias=True,
+           pad_type='zero', norm_type=None, act_type='relu'):
     '''
     Pixel shuffle layer
     (Real-Time Single Image and Video Super-Resolution Using an Efficient Sub-Pixel Convolutional
     Neural Network, CVPR17)
     '''
-    conv = conv_block(in_nc, out_nc * (upscale_factor ** 2), kernel_size, stride, bias=bias, \
-                        pad_type=pad_type, norm_type=None, act_type=None)
+    conv = conv_block(in_nc, out_nc * (upscale_factor ** 2), kernel_size, stride, bias=bias,
+                      pad_type=pad_type, norm_type=None, act_type=None)
     pixel_shuffle = nn.PixelShuffle(upscale_factor)
 
     n = norm(norm_type, out_nc) if norm_type else None
     a = act(act_type) if act_type else None
     return sequential(conv, pixel_shuffle, n, a)
 
+
 class ShortcutBlock(nn.Module):
-    #Elementwise sum the output of a submodule to its input
+    # Elementwise sum the output of a submodule to its input
     def __init__(self, submodule):
         super(ShortcutBlock, self).__init__()
         self.sub = submodule
@@ -256,11 +290,13 @@ class ShortcutBlock(nn.Module):
         tmpstr = tmpstr + modstr
         return tmpstr
 
+
 def sequential(*args):
     # Flatten Sequential. It unwraps nn.Sequential.
     if len(args) == 1:
         if isinstance(args[0], OrderedDict):
-            raise NotImplementedError('sequential does not support OrderedDict input.')
+            raise NotImplementedError(
+                'sequential does not support OrderedDict input.')
         return args[0]  # No sequential is needed.
     modules = []
     for module in args:
@@ -271,19 +307,18 @@ def sequential(*args):
             modules.append(module)
     return nn.Sequential(*modules)
 
-
     """
     Cascade Channel Attention Block, 3-3 style
     """
 
-    def __init__(self, nc, gc, kernel_size=3, stride=1, dilation=1, groups=1, reduction=16, \
-            bias=True, pad_type='zero', norm_type=None, act_type='prelu', mode='CNA', res_scale=1):
+    def __init__(self, nc, gc, kernel_size=3, stride=1, dilation=1, groups=1, reduction=16,
+                 bias=True, pad_type='zero', norm_type=None, act_type='prelu', mode='CNA', res_scale=1):
         super(CCAB, self).__init__()
         self.nc = nc
-        self.RCAB = nn.ModuleList([RCAB(gc, kernel_size, reduction, stride, dilation, groups, bias, pad_type, \
-                    norm_type, act_type, mode, res_scale) for _ in range(nc)])
-        self.CatBlocks = nn.ModuleList([conv_block((i + 2)*gc, gc, kernel_size=1, bias=bias, pad_type=pad_type, \
-            norm_type=norm_type, act_type=act_type, mode=mode) for i in range(nc)])
+        self.RCAB = nn.ModuleList([RCAB(gc, kernel_size, reduction, stride, dilation, groups, bias, pad_type,
+                                        norm_type, act_type, mode, res_scale) for _ in range(nc)])
+        self.CatBlocks = nn.ModuleList([conv_block((i + 2) * gc, gc, kernel_size=1, bias=bias, pad_type=pad_type,
+                                                   norm_type=norm_type, act_type=act_type, mode=mode) for i in range(nc)])
 
     def forward(self, x):
         pre_fea = x
@@ -292,6 +327,7 @@ def sequential(*args):
             pre_fea = torch.cat((pre_fea, res), dim=1)
             x = self.CatBlocks[i](pre_fea)
         return x
+
 
 @torch.no_grad()
 def default_init_weights(module_list, scale=1, bias_fill=0, **kwargs):
@@ -340,15 +376,15 @@ class ResBlock(nn.Module):
     """
 
     def __init__(self, num_feat=64, res_scale=1, pytorch_init=False, dilation=1,
-                        act_type='leakyrelu'):
+                 act_type='leakyrelu'):
         super().__init__()
         self.res_scale = res_scale
         padding = get_valid_padding(3, dilation)
 
-        self.conv1 = nn.Conv2d(num_feat, num_feat, 3, 1, bias=True, \
-                            padding=padding, dilation=dilation)
-        self.conv2 = nn.Conv2d(num_feat, num_feat, 3, 1, bias=True, \
-                            padding=padding, dilation=dilation)
+        self.conv1 = nn.Conv2d(num_feat, num_feat, 3, 1, bias=True,
+                               padding=padding, dilation=dilation)
+        self.conv2 = nn.Conv2d(num_feat, num_feat, 3, 1, bias=True,
+                               padding=padding, dilation=dilation)
         self.act = act(act_type) if act_type else None
 
         if not pytorch_init:
@@ -370,12 +406,14 @@ def kernel2d_conv(feat_in, kernel, ksize):
     N, kernels, H, W = kernel.size()
     pad_sz = (ksize - 1) // 2
 
-    feat_in = F.pad(feat_in, (pad_sz, pad_sz, pad_sz, pad_sz), mode="replicate")
+    feat_in = F.pad(feat_in, (pad_sz, pad_sz, pad_sz, pad_sz),
+                    mode="replicate")
     feat_in = feat_in.unfold(2, ksize, 1).unfold(3, ksize, 1)
     feat_in = feat_in.permute(0, 2, 3, 1, 5, 4).contiguous()
     feat_in = feat_in.reshape(N, H, W, channels, -1)
 
-    kernel = kernel.permute(0, 2, 3, 1).reshape(N, H, W, channels, ksize, ksize)
+    kernel = kernel.permute(0, 2, 3, 1).reshape(
+        N, H, W, channels, ksize, ksize)
     kernel = kernel.permute(0, 1, 2, 3, 5, 4).reshape(N, H, W, channels, -1)
     feat_out = torch.sum(feat_in * kernel, axis=-1)
     feat_out = feat_out.permute(0, 3, 1, 2).contiguous()
